d738a41218c77722b9c738007d581edf287e7797
Soogeun Park <bwmelon97@unist.ac.kr>
2019-07-05 13:31:20 +0900

    react 개발 환경 구축

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,42 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: ['@babel/polyfill', './src/js/main.js', './src/sass/main.scss'],
+    entry: ['@babel/polyfill', './src/js/index.js', './src/sass/main.scss'],
     output: {
         path: path.resolve(__dirname, 'dist/js'),
         filename: 'bundle.js'
     },
     plugins: [
-        new MiniCssExtractPlugin( {filename: '../css/style.css'} )
+        new MiniCssExtractPlugin( {filename: '../css/style.css'} ),
+        new HtmlWebPackPlugin ({
+            template: "./src/index.html",
+            filename: "../../index.html"
+        })
     ],
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\.(js|jsx)$/,
                 include: [
                     path.resolve(__dirname, 'src/js')
                 ],
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['@babel/preset-env'],
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
                         plugins: ['@babel/plugin-proposal-class-properties']
                     }
                 },
                 exclude: /node_modules/
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader: "html-loader",
+                    options: {minimize: true}
+                }
             },
             {
                 test: /\.scss$/,
@@ -35,6 +47,13 @@ module.exports = {
                     "sass-loader?outputStyle=expanded"
                 ],
                 exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader"
+                ]
             }
         ]
     },
